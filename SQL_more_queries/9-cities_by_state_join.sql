2220082935482d9de89505d97d838e105a966d04
Milky Kerenso <milkykerenso@gmail.com>
2024-01-11 17:44:01 +0300

    commit y

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,9 +1,9 @@
---A script that lists all cities contained in the database hbtn_0d_usa.
+-- creating database
 
 SELECT cities.id, cities.name, states.name
-       FROM cities, states
-       WHERE cities.state_id = states.id
-       ORDER BY cities.id;
+FROM cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id;
 
 
        
